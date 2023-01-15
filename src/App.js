962154016784f0b5831e8031d2f0ce56073ff042
rammotivaras <rkmotivaras77@gmail.com>
2023-01-15 18:32:35 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { FindUs, Chef, Gallery, Laurels, Header, AboutUs, Footer, Intro, SpecialMenu } from './container';
+
+import { Navbar } from './components';
+import './App.css';
+
+const App = () => (
+  <div>
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer />
+  </div>
+);
+
+export default App;
